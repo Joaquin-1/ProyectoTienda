7378e65330a31c7ac64990ef76e257c604de5b16
Joaquin-1 <joaquin.monge@iesdonana.org>
2022-12-09 10:32:43 +0100

    Welcome y rutas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 
-<nav x-data="{ open: false }" class="bg-black border-b border-gray-100 h-28">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 h-28">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
         <div class="flex justify-between h-16">
@@ -13,10 +13,11 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex text-xl">
-                <x-nav-link class="ml-[365px]" :href="route('register')" :active="request()->routeIs('register')">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex text-xl text-black">
+                <x-nav-link class="ml-[300px] " :href="route('register')" :active="request()->routeIs('register')">
                     {{ __('Register') }}
                 </x-nav-link>
+
                 <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
                     {{ __('Login') }}
                 </x-nav-link>
@@ -55,9 +56,10 @@
     <!-- Responsive Navigation Menu -->
     <div class="sm:bg-white flex" :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 mt-6 h-auto bg-white block border">
-            <x-nav-link class="text-black bg-white  w-full px-5 py-5 " :href="route('register')" :active="request()->routeIs('register')">
+            <x-nav-link class="text-[#4C3F2B] bg-white  w-full px-5 py-5 " :href="route('register')" :active="request()->routeIs('register')">
                 {{ __('Register') }}
             </x-nav-link>
+
             <x-nav-link  class="text-black bg-white  w-full px-5 py-5" :href="route('login')" :active="request()->routeIs('login')">
                 {{ __('Login') }}
             </x-nav-link>
@@ -74,17 +76,18 @@
         </div>
     </div>
 </nav>
-<div class="bg-black text-white text-5xl text-center pt-5 pb-3">
-    Products
+
+<div class="bg-[#B99A66] shadow text-center text-4xl text-black pt-7 pb-5 font-semibold">
+    CATÁLOGO
 </div>
 
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-[#B99A66] border-b border-gray-200">
                     <x-plantilla>
-                        <table class="table-auto">
+                        <table class="table-auto bg-white">
                             <tbody>
                                 @foreach ($productos as $producto)
                                     <tr class="border-2 border-grey-700">
@@ -105,15 +108,12 @@
                                             <a class="font-bold hover:text-orange-700" href="{{route('producto', $producto)}}"> More     </a>
                                         @endif
                                     </td>
-
                                         <td class="px-6 py-2">{{ $producto->precio }} &euro;</td>
                                         <td>
                                             <div class="text-sm text-gray-900 ">
-                                                <form action="{{ route('anadiralcarrito', $producto) }}" method="POST">
-                                                    @csrf
-                                                    @method('POST')
-                                                    <button type="submit" class="px-4 py-1 text-sm text-white bg-orange-500 rounded">Add to cart</button>
-                                                </form>
+
+                                                <button  onclick="location.href='/login'" class="px-4 py-1 text-sm text-white bg-orange-500 rounded">Add to cart</button>
+
                                             </div>
                                         </td>
                                         <td class="px-6 py-4">
@@ -132,32 +132,31 @@
         </div>
     </div>
 
-    <footer class="flex justify-center bg-black   w-full h-96 mt-1">
-        <div class="bg-black text-white mt-4 w-full h-5/6">
-            <div class="bg-black grid grid-rows-3 grid-flow-col gap-4  h-4/5">
+    <footer class="flex justify-center bg-white   w-full h-96 mt-1">
+        <div class="bg-[#4C3F2B] text-white mt-4 w-full h-5/6">
+            <div class="bg-[#B99A66] grid grid-rows-3 grid-flow-col gap-4  h-4/5">
                 <div class="absolute mt-10 flex ml-28">
-                    <img class="h-8" src="{{ URL('img/correo.png') }}" alt="">
-                    <p class="ml-2 text-2xl">gerardo@rentel.es</p>
+                    <img class="h-6" src="{{ URL('img/correo.png') }}" alt="">
+                    <p class="ml-2">betweenfilms@gmail.com</p>
                 </div>
-                <div class="flex mt-20 ml-28 col-span-full">
-                    <img class="mt-3 mr-1 w-8 h-8"
-                            src="{{ URL('img/youtube.png') }}" alt="facebook">
-                    <a class="mt-5" href="https://www.youtube.com/channel/UC-t6s_YDUsRrJRGg3CrNxlA">
-                         Gerardo Calado</a>
+                <div class="flex mt-24 ml-28 ">
+                    <img class=" mr-1 w-8 h-8"
+                            src="{{ URL('img/youtube.png') }}" alt="youtube">
+                    <a class="mt-1" href="https://www.youtube.com/@sonypictures/featured">
+                         BetweenFilms</a>
                 </div>
                 <div class="col-span-2 row-span-4 text-right mr-36 mt-10">
-                    <p class="text-2xl">Any problem?</p>
-                    <p class="text-xl mr-3 mt-2"> <a class="text-orange-500" href="#">Contact</a> with us
-                    </p>
+                    <p class="text-2xl">¿Tienes alguna duda? </p>
+                    <p class="text-xl mr-3 mt-2"> <a class="text-black" href="/contactos/create">Contacta</a> con nosotros </p>
                 </div>
             </div>
         </div>
-        <div class="w-full absolute mt-20">
+        <div class="w-full absolute mt-28">
             <p class="text-center text-3xl"><a class="hover:text-orange-500 text-white" href="{{ route('productos') }}"> Our products</a>
             </p>
         </div>
-        <div class="w-50 h-auto absolute mt-36">
-            <a href="{{ route('productos') }}"><img style="height: 20%; width: 20%; margin-left:40%" class="mt-3 mr-1"
+        <div class="w-50 h-auto absolute mt-40">
+            <a href="{{ route('productos') }}"><img class="h-20 w-32 mt-3 mr-1"
                 src="{{ URL('img/logo.png') }}" alt="logo"></a>
         </div>
     </footer>
