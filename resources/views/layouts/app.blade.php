ed9af46ef53f2c3b0edded92ea98cc011d1c16be
Joaquin-1 <joaquin.monge@iesdonana.org>
2023-11-20 11:26:38 +0100

    Cambios en el codigo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="font-sans antialiased">
 
-        <livewire:counter />
+
 
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
